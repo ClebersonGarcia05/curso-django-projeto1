f2786a3b6fe6f68bb46f4051f4b3dc52b3fe9b0d
Cleberson Garcia <cleberson.garcia051299@gmail.com>
2024-07-11 12:42:46 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Cleberson Garcia'
     })
-def sobre(request):
-    return render(request, 'recipes/contato.html')
-
-def contato(request):
-    return HttpResponse("Hello World")
